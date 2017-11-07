c130f52f5c904dec93581a1a5ed5ae0940b9d588
Andrew Madden <andrewmadden@Andrews-MacBook-Air.local>
2017-11-07 16:59:00 -0700

    commit from 11/7/17

--- colossal-rpg/main.js
+++ colossal-rpg/main.js
@@ -1,19 +1,32 @@
 var ask = require('readline-sync');
 
+const charName = ask.question('What is your name? ')
 var character = {
-  name: ask.question('What is your name? '),
+  name: charName,
   hp: 100,
   monstersDefeated: 0,
   hasSword: true,
   hasKey: false,
   hasArmor: false,
   hasSpear: false,
-  weapons: ["Knife"]
+  isGameOver: false,
+  weapons: ["knife"],
+  isAlive: true
 }
 
-const names = ["name1", "name2", "name3", "name4", "name5"];
+class MonsterCreator {
+  constructor(hp, name, type, weapon, ability) {
+    this.hp = hp;
+    this.name = name;
+    this.type = type;
+    this.weapon = weapon;
+    this.ability = ability
+  }
+}
+
+const names = ["Mongoose", "Snake", "Wolf", "Eagle", "Pupper"];
 const types = ["type1", "type2", "type3", "type4", "type5"];
-const weapons = ["weapon1", "weapon2", "weapon3", "weapon4", "weapon5"];
+const weapons = ["Slap!", "Bow Staff!", "Sword!!", "Spear!!", "MAGIC!!!"];
 const abilities = ["ability1", "ability2", "ability3", "ability4", "ability5"];
 function randomAttribute() {
   return Math.floor(Math.random() * 6);
@@ -34,64 +47,142 @@ let newMonster = new MonsterCreator(randomHp(), newName(), null, newWeapon, newA
 
 const enemyWeapons = ["Slap!", "Bow Staff!", "Sword!!", "Spear!!", "MAGIC!!!"]
 
-
-
-function enemyAttack() {
+const slap = () => {
+  console.log(`${newMonster.name} has slapped you!`);
+  character.hp = character.hp - Math.floor(Math.random() * 10);
+  console.log(`You lost up to 10hp.\n${character.name}: ${character.hp}HP`);
+}
+const bowStaff = () => {
+  console.log(`${newMonster.name} has hit you in the shin with a bow staff! -20hp`);
+  character.hp = character.hp - 20;
+}
+const sword = () => {
+  console.log("You have been pierced with a sword! - 35hp");
+  character.hp = character.hp - 35;
+}
+const enemyAttack = () => {
   let randomWeapon = enemyWeapons[randomAttribute()]
-  console.log("The enemy attacked you with ..." + randomWeapon);
+  console.log(randomWeapon);
+
+  if (character.hp <= 0) {
+    console.log("You have been defeated!");
+  }
   playerAttack();
 }
 
 function knifeAttack() {
-  newMonster.hp = newMonster.hp - 25;
+  newMonster.hp = newMonster.hp - Math.floor(Math.random() * 25);
   if (newMonster.hp <= 0) {
-    console.log(newMonster);
-    console.log("You have defeated the monster!");
+    console.log(`You have defeated ${newMonster.name}!`);
     newMonster = new MonsterCreator(randomHp(), newName(), null, newWeapon, newAbility);
     character.monstersDefeated++;
-    if (character.monstersDefeated >= 3) {
-      bossFight();
-    }
-    walk();
+    character.monstersDefeated >= 3 ? bossFight(): walk();
   } else {
-    console.log(newMonster);
+    battleStats();
     enemyAttack();
   }
 }
 
 function spearAttack() {
-  newMonster.hp = newMonster.hp - 50;
+  newMonster.hp = newMonster.hp - Math.floor(Math.random() * 50);
   if (newMonster.hp <= 0) {
     console.log(newMonster);
     console.log("You have defeated the monster!");
     character.monstersDefeated++;
-    monstersDefeated > 3 ? "You have defeated three monsters. Prepare for your final battle":
-    walk();
-    if (character.monstersDefeated >= 3) {
-      bossFight();
-    }
+    character.monstersDefeated > 3 ? bossFight(): walk();
   } else {
-    console.log(newMonster);
+    battleStats();
     enemyAttack();
   }
 }
 
-function playerAttack() {
-  var chosenWeapon = ask.keyInSelect(character.weapons, "What weapon would you like to use?");
-  if (chosenWeapon === 0) {
-    knifeAttack();
-    console.log("You attacked with your knife.  -25hp");
-  } else if (chosenWeapon === 1) {
-    spearAttack();
-    console.log("You attacked with your spear.  -40hp");
+function bowStaffAttack() {
+  newMonster.hp = newMonster.hp - Math.floor(Math.random() * 75);
+  if (newMonster.hp <= 0) {
+    console.log(newMonster);
+    console.log("You have defeated the monster!");
+    character.monstersDefeated++;
+    character.monstersDefeated > 3 ? bossFight(): walk();
+  } else {
+    battleStats();
+    enemyAttack();
   }
 }
 
+function swordAttack() {
+  newMonster.hp = newMonster.hp - Math.floor(Math.random() * 60);
+  if (newMonster.hp <= 0) {
+    console.log(newMonster);
+    console.log("You have defeated the monster!");
+    character.monstersDefeated++;
+    character.monstersDefeated > 3 ? bossFight(): walk();
+  } else {
+    battleStats();
+    enemyAttack();
+  }
+}
+
+function magicAttack() {
+  newMonster.hp = newMonster.hp - Math.floor(Math.random() * 130);
+  if (newMonster.hp <= 0) {
+    console.log(newMonster);
+    console.log("You have defeated the monster!");
+    character.monstersDefeated++;
+    character.monstersDefeated > 3 ? bossFight(): walk();
+  } else {
+    battleStats();
+    enemyAttack();
+  }
+}
+
+const battleStats = () => {
+  console.log(`${newMonster.name}: \nHP: ${newMonster.hp}\n
+              \n\\\\\\\\\\VS\\\\\\\\\\
+              \n${character.name}: \nHP: ${character.hp}`);
+}
+
+
+
+function playerAttack() {
+  var chosenWeapon = ask.keyInSelect(character.weapons, "What weapon would you like to use?");
+  for (let i = 0; i < character.weapons.length; i++) {
+    if (chosenWeapon === character.weapons.indexOf("knife")) {
+      console.log(`You attacked ${newMonster.name} with your knife!`);
+      knifeAttack();
+    } else if (chosenWeapon === character.weapons.indexOf("Bow Staff!")){
+      console.log(`You attacked ${newMonster.name} with your Bow Staff!`);
+      bowStaffAttack();
+    } else if (chosenWeapon === character.weapons.indexOf("Sword!!")) {
+      console.log(`You attacked ${newMonster.name} with you Sword!`);
+      swordAttack();
+    } else if (chosenWeapon === character.weapons.indexOf("Spear!!")) {
+      console.log(`You attacked ${newMonster.name} with your Spear!`);
+      spearAttack();
+    } else if (chosenWeapon === character.weapons.indexOf("MAGIC!!!")) {
+      console.log(`You attacked ${newMonster.name} with MAGIC!!!!!`);
+      magicAttack();
+    }
+
+
+}
+
+
+
+  // if (chosenWeapon === 0) {
+  //   console.log("You attacked with your knife.  -20hp");
+  //   knifeAttack();
+  // } else if (chosenWeapon === 1) {
+  //   console.log("You attacked with your spear.  -40hp");
+  //   spearAttack();
+  // }
+}
+
 function fight(){
-  console.log("You have chosen to fight!  Here is your matchup.");
-  console.log(newMonster);
-  console.log("\\\\\\\\\\VS\\\\\\\\\\");
-  console.log(character);
+  console.log(`You have chosen to fight!  Here is your matchup.`);
+  console.log(`${newMonster.name}: \nHP: ${newMonster.hp}\n\n\\\\\\\\\\VS\\\\\\\\\\
+              \n${character.name}: \nHP: ${character.hp}`);
+  // console.log("\\\\\\\\\\VS\\\\\\\\\\");
+  // console.log(`${character.name}: \nHP: ${character.hp}`);
   playerAttack();
 }
 
@@ -99,9 +190,10 @@ function bossFight(){
   console.log("You have reached the boss.");
 }
 
+
 function run() {
   console.log("You chose to run")
-
+  const randDeath = (Math.random() * 100) > 50 ? gameOver(): console.log("You just escaped");
 }
 
 function runOrFight() {
@@ -118,9 +210,23 @@ const randomFight = function(){
   let willFight = Math.random() * 5 > 1 ? walk(): runOrFight();
   // console.log(willFight);
 }
+const printInventory = () => {
+  console.log(character);
+  console.log("Here are your characters stats.")
+}
+ function walkQuestion() {
+   let input = ask.question('Enter "W" to walk: \nOr enter "Print" see inventory:\n');
+   if (input.toLowerCase() === "print") {
+     printInventory();
+   }
+   input.toUpperCase() === "W" ? randomFight():
+     console.log("You are delaying the inevitable.");
+     walkQuestion();
+ }
+
 let distanceWalked = 0;
-function walk() {
-  distanceWalked++
+const walk = () => {
+  distanceWalked++;
   if (distanceWalked % 3 === 0) {
     console.log("You found an item!");
     if (!character.hasKey) {
@@ -128,22 +234,16 @@ function walk() {
     } else if (!character.hasArmor) {
       character.hasArmor = true;
     } else if (!character.hasSpear) {
-      character.weapons.push("Spear");
+      character.weapons.push(weapons[randomAttribute()]);
     }
-  }
-  ask.question('Enter "W" to walk: ');
-  randomFight();
+  } walkQuestion();
 }
 
-walk();
-console.log(distanceWalked)
+function gameOver() {
+  console.log("Game Over...You were not fast enough");
+  character.isAlive = false;
+}
 
-
-
-function MonsterCreator(hp, name, type, weapon, ability){
-  this.hp = hp;
-  this.name = name;
-  this.type = type;
-  this.weapon = weapon;
-  this.ability = ability
+while (character.isAlive === true) {
+  walk();
 }
