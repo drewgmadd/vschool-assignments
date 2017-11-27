67dfae4c675cc533f346fa658f3c053862cc3313
Andrew Madden <andrew12madden@gmail.com>
2017-11-27 07:00:48 -0700

    commit from 11/27/17

--- exercises/random-color/src/Color.js
+++ exercises/random-color/src/Color.js
@@ -1,8 +1,19 @@
 import React, {Component} from "react";
 import ColorBlock from "./ColorBlock";
+import axios from "axios";
 
 class Color extends Component {
-
+constructor() {
+  super();
+  this.state = {
+    color: ''
+  }
+}
+componentDidUpdate() {
+  axios.get("http://www.colr.org/json/color/random").then(response => {
+    this.setState({color: response.data})
+  })
+}
   render(){
   return (
     <div>
--- projects/high-score/src/Score.js
+++ projects/high-score/src/Score.js
@@ -9,7 +9,6 @@ function Score(props) {
       <h3 className="text">score: {props.score.score}</h3>
       <h3 className="text">Date: {props.score.date}</h3>
       <h3 className="text">Trash Talk: {props.score.talkTrash}</h3>
-      <button className="btn">delete</button>
     </div>
   )
 }
