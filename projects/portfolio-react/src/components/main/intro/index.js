7eb3b5c2e275264b494478834fcf734f3bdcf476
Andrew Madden <andrew12madden@gmail.com>
2018-03-29 12:47:29 -0600

    portfolio cleanup

--- projects/portfolio-react/src/components/main/intro/index.js
+++ projects/portfolio-react/src/components/main/intro/index.js
@@ -5,7 +5,7 @@ function Intro() {
     <main className="about-block">
       <p className="about-text-title">About</p>
       <div className="headshot"></div>
-      <p className="about-text">Born and rased in Salt Lake City, UT, I have a passion for the mountains and outdoors that has only recently become rivaled by my passion for web development and the associated problem solving and learning process.  I began my coding journey spring 2017 leading me to a full stack JavaScript certificate from Vschool in Salt Lake City.  I  am positive, hard-working and have a passion for learning and implementing various technologies.  Check out my progress on <a className="about-link" href="https://github.com/drewgmadd">github!</a></p>
+      <p className="about-text">Born and raised in Salt Lake City, UT, I have a passion for the mountains and outdoors that has only recently become rivaled by my passion for web development and the associated problem solving and learning process.  I began my coding journey spring 2017 leading me to a full stack JavaScript certificate from Vschool in Salt Lake City.  I  am positive, hard-working and have a passion for learning and implementing various technologies.  Check out my progress on <a className="about-link" href="https://github.com/drewgmadd">github!</a></p>
     </main>
   )
 }
